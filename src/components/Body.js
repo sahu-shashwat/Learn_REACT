fd05f6fec57ca8d22022a171ab290ba303aacdf6
Shashwat Sahu <shashwatsahu780@gmail.com>
2024-02-26 23:30:51 +0530

    Ep6 1 finished

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,32 +2,35 @@ import Resturentcard from "./Resturentcard";
 import resList from "../utils/Mokedata";
 import { useState,useEffect } from "react";
 import Shimmer from "./Shimmer";
+
 const Body =()=>{
    const [listOfres, setlistOfres]=useState([]);
    const reslist2=resList
-
+   const [searchText,setsearchText]=useState('')
+   const[filterResList,setfilterResList]=useState([])
    useEffect(()=>{
     fetchData();
-   },[])
-   
+   },[])  
    const fetchData = async() =>{
-      const data= await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
+      // const data= await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING');
 
-      const json= await data.json();
-      console.log(json)
-         
+      // const json= await data.json();
+      // console.log(json)
+      setfilterResList(reslist2)
       setlistOfres(reslist2)
    }; 
   
 
    // Conditonsal rendering
-  if (listOfres.length ===0){
-   return <Shimmer/>
-  }
-
-   return (    
+//   if (listOfres.length ===0){
+//    return <Shimmer/>
+//   }
+   
+   return listOfres.length ===0 ?(
+      <Shimmer></Shimmer>
+   ) : (    
     <div className="body">
-        <div className="filter">
+        <div className="filter"> 
              <button className="filter-btn"
                onClick={()=>{
                 const filteredlist=listOfres.filter(
@@ -44,9 +47,28 @@ const Body =()=>{
                 setlistOfres(filteredlist2)
               }}
              >food under 300 rupies</button>
+             <dev className='search'>
+                <input type="text"
+                 className="search-box" 
+                 value={searchText}
+                 onChange={(e)=>{
+                  setsearchText(e.target.value)
+                 }}
+                />
+                <button
+                 onClick={()=>{
+                  const filteredres=listOfres.filter(
+                     (res)=>res.data.name.toLowerCase().includes(searchText.toLowerCase())
+                     )
+                   setfilterResList(filteredres)
+                 }}
+                >
+                  search
+                  </button>
+             </dev>  
         </div>
         <div className="res-container">
-         {listOfres.map((rnt)=>(
+         {filterResList.map((rnt)=>(
             <Resturentcard key={rnt.data.id} resdata={rnt}/>
          ))}
         </div>
--- src/components/Headet.js
+++ src/components/Headet.js
@@ -1,7 +1,10 @@
 import { LOGO_URL } from "../utils/constants"
+import { useState } from "react"
+const Header =()=>{
 
-const Header =()=>(
-    <div className="header">
+    const [loginbtn,setloginbtn]=useState('Login')
+return (
+<div className="header">
         <div className="logo-continer">
             <img className="logo" src={LOGO_URL}/>
         </div>
@@ -11,9 +14,18 @@ const Header =()=>(
                 <li>About</li>
                 <li>Contact Us</li>
                 <li>Cart</li>
+                <button className="Login"
+                 onClick={()=>{
+                    loginbtn=='Login'
+                    ?setloginbtn('Logout')
+                    :setloginbtn('Login');
+                 }} 
+                >
+                {loginbtn}
+                </button>
             </ul>
         </div>
 
     </div>
-)
+)}
 export default Header
